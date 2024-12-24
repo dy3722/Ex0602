4b8e154920a4305c3d59d5ea78c441f5d0a08222
dy3722 <dy3722@bs.amalnet.k12.il>
2024-12-24 22:19:30 +0200

    first task

--- app/src/main/java/com/example/ex0602/MainActivity.java
+++ app/src/main/java/com/example/ex0602/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ex0602;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +13,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+     Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.button);
+    }
+
+    public void gogo(View view) {
+        btn.setText("Oh, yea, I’ve been clicked!");
     }
 }
